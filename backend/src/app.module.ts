3d201394ac01b8659980e86ea4e325e56d18e3f4
anonfedora <musaeleazar090@gmail.com>
2024-04-17 09:29:11 +0100

    initial commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,20 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { Contact } from 'entities/contact.entity';
-import { ContactService } from './contact.service';
-import { ContactsController } from './contacts/contacts.controller';
-
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      database: 'nestngdb',
-      username: 'root',
-      password: 'jb395566',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-    }),
-    TypeOrmModule.forFeature([Contact]),
-  ],
-  controllers: [AppController, ContactsController],
-  providers: [AppService, ContactService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: "mysql",
+            database: "nestngdb",
+            username: "root",
+            password: "jb395566",
+            entities: [__dirname + "/**/*.entity{.ts,.js}"],
+            synchronize: true
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
-export class AppModule { }
+export class AppModule {}
